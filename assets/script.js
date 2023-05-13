bcbdc5034e5ab3bb5b55daf624ce5e635c56734e
Loic00999 <loic.minet.pro@outlook.fr>
2023-05-13 14:51:27 +0200

    Étape 2 : Ajoutez des Event Listeners sur les flèches

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,49 @@
+	/**
+	 * Premiers pas sur le langage JavaScript
+	 * Projet OpennClassRooms n°5 par Loïc M
+	 */
+/**
+ * Étape 2 : Ajoutez des Event Listeners sur les flèches
+ */
+	//Clic flèche gauche bouton souris gauche
+function clickArrowLeft(arrow_left){
+	alert("Click Left");
+	console.log(arrow_left);
+}
+let arrow_left = document.getElementById("arrow_left");
+arrow_left.addEventListener("click", clickArrowLeft);
+	//Clic flèche gauche bouton souris droit
+function clickArrowLeftLeftClick(arrow_left){
+	alert("Click Left Left-Click");
+	console.log(arrow_left);
+}
+arrow_left = document.getElementById("arrow_left");
+arrow_left.addEventListener("contextmenu", clickArrowLeftLeftClick);
+	//Clic flèche droite bouton souris gauche
+function clickArrowRight(arrow_right){
+	alert("Click Right");
+	console.log(arrow_right);
+}
+let arrow_right = document.getElementById("arrow_right");
+arrow_right.addEventListener("click", clickArrowRight);
+	//Clic flèche droite bouton souris droit
+function clickArrowRightRightClick(arrow_right){
+	alert("Click Right Right-Click");
+	console.log(arrow_right);
+}
+arrow_right = document.getElementById("arrow_right");
+arrow_right.addEventListener("contextmenu", clickArrowRightRightClick);
+/**
+ * Étape 3 : Ajoutez des bullet points au slider
+ */
+
+/**
+ * Étape 4 : Modifiez le slide au clic sur le bouton
+ */
+
+/**
+ * Étape 5 : Mettez en place le défilement infini sur le carrousel
+ */
 const slides = [
 	{
 		"image":"slide1.jpg",
