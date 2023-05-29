e51dafd021dedb233f348b74cee3fca0d40ac6af
Loic00999 <loic.minet.pro@outlook.fr>
2023-05-29 22:25:59 +0200

    tagLine

--- assets/script.js
+++ assets/script.js
@@ -19,46 +19,54 @@ const slides = [
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
+
 /* *** Mise à jour du code HTML via JavaScript *** */
 const removeImg = document.querySelector(".banner-img");
 removeImg.remove();
+
 function createElements() {
+  const banner = document.querySelector("#banner");
   const divBannerSlides = document.createElement("div");
   divBannerSlides.setAttribute("id", "banner-slides");
   divBannerSlides.classList.add("banner-img");
-  document.querySelector("#banner").appendChild(divBannerSlides);
+  banner.appendChild(divBannerSlides);
   const arrowLeft = document.createElement("img");
   arrowLeft.setAttribute("id", "arrow_left");
   arrowLeft.classList.add("arrow");
   arrowLeft.classList.add("arrow_left");
-  arrowLeft.src = "./assets/images/arrow_left.png";
+  arrowLeft.setAttribute("src", "./assets/images/arrow_left.png");
   arrowLeft.setAttribute("alt", "arrow left");
-  document.querySelector("#banner").appendChild(arrowLeft);
+  banner.appendChild(arrowLeft);
   const arrowRight = document.createElement("img");
   arrowRight.setAttribute("id", "arrow_right");
   arrowRight.classList.add("arrow");
   arrowRight.classList.add("arrow_right");
-  arrowRight.src = "./assets/images/arrow_right.png";
+  arrowRight.setAttribute("src", "./assets/images/arrow_right.png");
   arrowRight.setAttribute("alt", "arrow right");
-  document.querySelector("#banner").appendChild(arrowRight);
-  document.querySelector("p").classList.add("tag-line-label");
+  banner.appendChild(arrowRight);
+  document.querySelector("p").classList.add("taglinelabel");
 }
 createElements();
+
 /* *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** */
 function addSlideInBanner() {
   const bannerSlides = document.getElementById("banner-slides");
-  slides.forEach(function (slide, index) {
-    console.log(slide);
+  slides.forEach(function (slide, index) {    
     const divImg = document.createElement("img");
+    divImg.setAttribute("id", index);
     divImg.classList.add("banner-img");
-    divImg.classList.add("picSlide"); /**/
+    divImg.classList.add("picSlide");
     divImg.setAttribute("src", slide.image);
     divImg.setAttribute("alt", slide.tagLine);
-    divImg.setAttribute("id", index);
     bannerSlides.appendChild(divImg);
+    if (index != 0) {
+      divImg.style.display = "none";
+    }
+    console.log(slide);
   });
 }
 addSlideInBanner();
+
 /* *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** */
 function createSpansDot() {
   let slide;
@@ -73,74 +81,94 @@ function createSpansDot() {
   }
 }
 createSpansDot();
+
 /* *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** */
+/* *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** */
+/* *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** */
+
 /* *** Action addEventListener *** */
+let slideIndex = 0;
+let tagLineIndex = 0;
+const updateTagLine = document.querySelector(".taglinelabel");
+
 /* Arrows */
+/* Arrow Right */
 function clickArrowRight() {
-  showSlides(slideIndex += 1);
-  updateTagLine();
-  console.log(updateTagLine);
+  showSlides();
+  upGradeTagLine(tagLineIndex += 1);
+  console.log(clickArrowRight);
 }
-let arrow_right = document.getElementById("arrow_right");
+const arrow_right = document.getElementById("arrow_right");
 arrow_right.addEventListener("click", clickArrowRight);
+/* Arrow Left */
 function clickArrowLeft() {
   showSlides(slideIndex += -1);
-  updateTagLine();
-  console.log(updateTagLine);
+  upGradeTagLine(tagLineIndex += -1);
+  console.log(clickArrowLeft);
 }
-let arrow_left = document.getElementById("arrow_left");
+const arrow_left = document.getElementById("arrow_left");
 arrow_left.addEventListener("click", clickArrowLeft);
-/* *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** */
-/* TagLine */
-function updateTagLine() {
-  const labeltagLine = document.querySelector(".tag-line-label");
-  let slideStart = 0;
-  let tagLineUpgrade = slides[slideStart].tagLine;
-  labeltagLine.innerHTML = tagLineUpgrade;
-}
-updateTagLine();
-/* *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** */
-/* Dots */
-function clickDot1() {
-  showSlides(slideIndex = 1);
-}
-let current1 = document.getElementById("current-0");
-current1.addEventListener("click", clickDot1);
-function clickDot2() {
-  showSlides(slideIndex = 2);
-}
-let current2 = document.getElementById("current-1");
-current2.addEventListener("click", clickDot2);
-function clickDot3() {
-  showSlides(slideIndex = 3);
-}
-let current3 = document.getElementById("current-2");
-current3.addEventListener("click", clickDot3);
-function clickDot4() {
-  showSlides(slideIndex = 4);
-}
-let current4 = document.getElementById("current-3");
-current4.addEventListener("click", clickDot4);
+
 /* *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** */
 /* Carousel */
-let slideIndex = 1;
-function showSlides(n) {
+function showSlides() {
   let i;
   let slides = document.getElementsByClassName("picSlide");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 0;
   }
-  if (n < 1) {
+  if (slideIndex < 1) {
     slideIndex = slides.length;
   }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" dot_selected", "");
   }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " dot_selected";
-  console.log(n);
+  console.log(slideIndex);
+  slides[slideIndex -1].style.display = "block";
+  dots[slideIndex -1].className += " dot_selected";
+ 
+  console.log(showSlides);
 }
+
+function upGradeTagLine() {
+  let i;
+  let tagLine = document.getElementsByClassName("taglinelabel");
+  for (i = 0; i < tagLine.length; i++) {
+    tagLine[i].innerHTML = slides[tagLineIndex].tagLine;
+  }
+  slides[tagLineIndex -1].innerHTML = slides[tagLineIndex].tagLine;  
+  console.log(upGradeTagLine);
+}
+
+/* *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** */
+/* Dots */
+function clickDot1() {
+  showSlides(slideIndex = 1);
+  updateTagLine.innerHTML = slides[0].tagLine;
+}
+const current1 = document.getElementById("current-0");
+current1.addEventListener("click", clickDot1);
+
+function clickDot2() {
+  showSlides(slideIndex = 2);
+  updateTagLine.innerHTML = slides[1].tagLine;
+}
+const current2 = document.getElementById("current-1");
+current2.addEventListener("click", clickDot2);
+
+function clickDot3() {
+  showSlides(slideIndex = 3);
+  updateTagLine.innerHTML = slides[2].tagLine;
+}
+const current3 = document.getElementById("current-2");
+current3.addEventListener("click", clickDot3);
+
+function clickDot4() {
+  showSlides(slideIndex = 4);
+  updateTagLine.innerHTML = slides[3].tagLine;
+}
+const current4 = document.getElementById("current-3");
+current4.addEventListener("click", clickDot4);
